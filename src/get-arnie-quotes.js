8668d9a0aa7583d63fd58ea8a63fa21cd94508d4
John Daniel Sevilla <jdsevilla1996@gmail.com>
2021-06-10 21:20:53 +1000

    Passes all tests

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,14 @@
+
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  return (await Promise.all(urls.map(url => httpGet(url))))
+    .map(response => ({
+      [`${response.status == 200 ? 'Arnie Quote' : 'FAILURE'}`]: JSON.parse(response.body).message
+    }));
 };
 
 module.exports = {
   getArnieQuotes,
 };
+
